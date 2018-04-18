86ab9e22149604b165b4ebbd3449e05201c80dd9
tesseradecades <nwe8978@rit.edu>
2018-04-18 17:27:01 -0400

    added index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    if(/*logged in*/ $_SESSION["username"]==""){
+        ?>
+        <script>
+            window.location.replace("feed.php");
+        </script>
+        <?php
+    }else/*not logged in*/{
+        ?>
+        <script>
+            window.location.replace("signin.php");
+        </script>
+        <?php
+    }
+?>
