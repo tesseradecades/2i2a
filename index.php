4d6e5561ac853a4d31a2e0d2b6530094832e6310
tesseradecades <nwe8978@rit.edu>
2018-04-18 18:09:29 -0400

    testing

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
     session_start();
-    if(/*logged in*/ $_SESSION["username"] != ""){
+    var_dump($_SESSION["username"]);
+    /*if( $_SESSION["username"] != ""){//logged in
         ?>
         <script>
             window.location.replace("feed.php");
         </script>
         <?php
-    }else/*not logged in*/{
+    }else{//not logged in
         ?>
         <script>
             window.location.replace("signin.php");
         </script>
         <?php
-    }
+    }*/
 ?>
