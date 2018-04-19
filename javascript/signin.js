18abf91e47da00b7da5f0bc0c4075736a254ff13
tesseradecades <nwe8978@rit.edu>
2018-04-19 10:34:00 -0400

    stay's logged in on refresh

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,30 +1,51 @@
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
 function signin(){
-    $("#loggedOutDropdown").hide();
     var username = $("#inputUsername").val();
-    console.log(username);
-    $("#userDropdown").text(username);
-    $("#loggedInDropdown").show();
-    $('#lastVisited').text(localStorage['lastVisited']);
-    var d = new Date();
-    localStorage['nextVisited'] = "Last Visited "+d.toLocaleDateString()+" "+d.toLocaleTimeString();
+    if(username){
+        $("#loggedOutDropdown").hide();
+        
+        localStorage['username'] = username;
+        $("#userDropdown").text(localStorage['username']);
+        $("#loggedInDropdown").show();
+        $('#lastVisited').text(localStorage['lastVisited']);
+        var d = new Date();
+        localStorage['nextVisited'] = "Last Visited "+d.toLocaleDateString()+" "+d.toLocaleTimeString();
+
+        $("#inputUsername").val("");
+        $("#inputPassword").val("");
+    }else{
+        alert("Please fill out the username field.");
+    }
+    
+    
 }
 
 function signOut(){
     $("#loggedOutDropdown").show();
     $("#userDropdown").text("");
+    localStorage['username'] = "";
     $("#loggedInDropdown").hide();
     localStorage['lastVisited']=localStorage['nextVisited'];
     $('#lastVisited').text("");
+    $("#inputUsername").val("");
+    $("#inputPassword").val("");
+    
 }
+$(document).ready(function(){
+    if(localStorage['username']){
+        $("#loggedOutDropdown").hide();
+        $("#userDropdown").text(localStorage['username']);
+        $("#loggedInDropdown").show();
+        $('#lastVisited').text(localStorage['lastVisited']);
+        
+        $("#inputUsername").val("");
+        $("#inputPassword").val("");
+    }else{
+        $("#loggedOutDropdown").show();
+        $("#userDropdown").text("");
+        localStorage['username'] = "";
+        $("#loggedInDropdown").hide();
+        $('#lastVisited').text("");
+        $("#inputUsername").val("");
+        $("#inputPassword").val("");
+    }
+});
