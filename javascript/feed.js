8523650ac6f47e30b4265b4992b30a3a46956fbf
tesseradecades <nwe8978@rit.edu>
2018-04-18 22:08:26 -0400

    combined feed, still not sorted

--- javascript/feed.js
+++ javascript/feed.js
@@ -13,29 +13,31 @@ function init(url){
 
     document.querySelector("#content").innerHTML = "<b>Loading news...</b>";
     $("#content").fadeOut(250);
-    document.querySelector("#content2").innerHTML = "<b>Loading news...</b>";
+    /*document.querySelector("#basketballcontent").innerHTML = "<b>Loading news...</b>";
     $("#content2").fadeOut(250);
-    document.querySelector("#content3").innerHTML = "<b>Loading news...</b>";
-    $("#content3").fadeOut(250);
+    document.querySelector("#baseballcontent").innerHTML = "<b>Loading news...</b>";
+    $("#content3").fadeOut(250);*/
     //fetch the data
-    $.get(url).done(function(data){xmlLoadedBase(data,"");});
-    $.get(url2).done(function(data){xmlLoadedBase(data,"2");});
-    $.get(url3).done(function(data){xmlLoadedBase(data,"3");});
+    $.get(url).done(function(data){xmlLoaded(data,"football");});
+    $.get(url2).done(function(data){xmlLoaded(data,"basketball");});
+    $.get(url3).done(function(data){xmlLoaded(data,"baseball");});
     //alert(data);
 }
 
 function filterFeed(){
     $('.filter').toArray().forEach(element => {
         console.log(element['name']);
+        var fil = 'div.item.'+element['name']+'item';
+        
         if(element.checked == true){
-            $("#"+element['name']+"Div").show();
+            $(fil).show();
         }else{
-            $("#"+element['name']+"Div").hide();
+            $(fil).hide();
         }
     });
 }
 
-function xmlLoaded(obj){
+function xmlLoaded(obj,n){
     console.log("obj = " +obj);
     var items = obj.querySelectorAll("item");
     
@@ -57,54 +59,31 @@ function xmlLoaded(obj){
         var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
         
         //present the item as HTML
-        var line = '<div class="item">';
+        var line = '<div class="item '+n+'item">';//+'<td><img src="assets/pointed-star-silhouette_318-35987.jpg" width="16" height="16" onclick="favorite(this)"></td><td>';
         line += "<h2>"+title+"</h2>";
-        line += '<p><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a></p>';
-        //title and description are always the same (for some reason) so I'm only including one
-        //line += "<p>"+description+"</p>";
+        line += '<p><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a></p></td>';
         line += "</div>";
         
         html += line;
     }
-    document.querySelector("#content").innerHTML = html;
+    if(document.querySelector("#content").innerHTML == "<b>Loading news...</b>"){
+        document.querySelector("#content").innerHTML = html;
+    }else{
+        document.querySelector("#content").innerHTML += html;
+    }
+    //document.querySelector("#"+n+"content").innerHTML = html;
         
     $("#content").fadeIn(1000);
 
 }
 
-function xmlLoadedBase(obj,n){
-    console.log("obj = " +obj);
-    var items = obj.querySelectorAll("item");
-    
-    //show the logo
-    var image = obj.querySelector("image")
-    var logoSrc = image.querySelector("url").firstChild.nodeValue;
-    var logoLink = image.querySelector("link").firstChild.nodeValue;
-    $("#logo"+n).attr("src",logoSrc);
-    
-    //parse the data
-    var html = "";
-    for (var i=0;i<items.length;i++){
-        //get the data out of the item
-        var newsItem = items[i];
-        var title = newsItem.querySelector("title").firstChild.nodeValue;
-        console.log(title);
-        var description = newsItem.querySelector("description").firstChild.nodeValue;
-        var link = newsItem.querySelector("link").firstChild.nodeValue;
-        var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
-        
-        //present the item as HTML
-        var line = '<div class="item">';
-        line += "<h2>"+title+"</h2>";
-        line += '<p><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a></p>';
-        //title and description are always the same (for some reason) so I'm only including one
-        //line += "<p>"+description+"</p>";
-        line += "</div>";
-        
-        html += line;
-    }
-    document.querySelector("#content"+n).innerHTML = html;
-        
-    $("#content"+n).fadeIn(1000);
+function favorite(obj){
+    console.log(obj['src']);
+    var unfav = "assets/pointed-star-silhouette_318-35987.jpg";
+    if(obj['src'] == unfav){
+        obj['src'] = "assets/Gold_Star.png";
 
+    }else{
+        obj['src'] = unfav;
+    }
 }
